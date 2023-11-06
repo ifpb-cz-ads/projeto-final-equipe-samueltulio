17a6b9081b493757aa0f913e7701e54f5b3f542c
Samuel Andrade de Araújo <andradedearaujo100@gmail.com>
2023-11-06 09:12:15 -0300

    Remodelando as classes para camada model.

---
+++ Model/Pessoa.java
@@ -0,0 +1,11 @@
+package model.Model;
+
+import java.util.Date;
+
+public class Pessoa {
+   private String email;
+   private String nome;
+   private String cpf;
+   private String matricula;
+   private Date dataNascimento;
+}

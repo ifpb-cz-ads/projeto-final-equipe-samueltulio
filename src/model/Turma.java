a717f9040cdd6b3c07ef45b2ef4d23595db0296b
flaviotulioalmeida <f.tulioalmeida@gmail.com>
2023-11-18 08:38:00 -0300

    Adicionando arquivos

--- src/model/Turma.java
+++ src/model/Turma.java
@@ -3,6 +3,7 @@ package model;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Set;
 
 public class Turma implements Serializable {
@@ -20,9 +21,24 @@ public class Turma implements Serializable {
                 '}';
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Turma turma)) return false;
+        return anoLetivo == turma.anoLetivo && Objects.equals(serie, turma.serie) && Objects.equals(alunos, turma.alunos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serie, anoLetivo, alunos);
+    }
+
     public Turma(String serie, int anoLetivo) {
         this.serie = serie;
         this.anoLetivo = anoLetivo;
+
+
         alunos = new Set<Aluno>() {
             @Override
             public int size() {
@@ -98,6 +114,9 @@ public class Turma implements Serializable {
             public int hashCode() {
                 return 0;
             }
+
+
         };
     }
+
 }
